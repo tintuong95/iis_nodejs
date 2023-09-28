f102236c1d603cb2bc0efa63a2af4aa8d37cb376
tintuong95 <tintuong95@gmail.com>
2023-09-28 15:40:14 +0700

    d

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/api', (req, res) => {
+    res.send('Hello World api')
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
